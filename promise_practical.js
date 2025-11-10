d6b353b48f27f137942fe2058a6d67571d5e75da
Santhosh <santhosh12san@gmail.com>
2025-11-10 08:26:00 -0800

    promise practical example

---
+++ promise_practical.js
@@ -0,0 +1,59 @@
+// ------------------------------- e-commerce example--------------------------------------------------------
+// function placeOrder() {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve("Order Placed"), 1000);
+//     });
+// }
+
+// function processPayment() {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve("Payment Successful"), 1500);
+//     });
+// }
+
+// function shipProduct() {
+//     return new Promise(resolve => {
+//         setTimeout(() => resolve("Product Shipped"), 2000);
+//     });
+// }
+
+// placeOrder()
+//   .then(res => {
+//     console.log(res);
+//     return processPayment();
+//   })
+//   .then(res => {
+//     console.log(res);
+//     return shipProduct();
+//   })
+//   .then(res => console.log(res))
+//   .catch(err => console.log(err));
+
+
+//--------------------------Manufacturing Industry--------------------------------------
+
+// function getMachineStatus() {
+//     return new Promise((resolve, reject) => {
+//         setTimeout(() => {
+//             let status = "RUNNING";  
+//             resolve("Machine Status: " + status);
+//         }, 2000);
+//     });
+// }
+
+// getMachineStatus()
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+
+function fetchAccountDetails(accountId) {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ accountId, balance: 50000, status: "Active" });
+        }, 1500);
+    });
+}
+
+fetchAccountDetails(101)
+    .then(info => console.log(info))
+    .catch(err => console.log(err));
