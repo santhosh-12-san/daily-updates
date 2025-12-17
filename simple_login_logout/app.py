3a599840309c24b7db4e3c4bbd1921c5a952b9a9
Santhosh <santhosh12san@gmail.com>
2025-12-18 02:00:18 +0530

    Remove simple_login_logout folder

--- simple_login_logout/app.py
+++
@@ -1,8 +0,0 @@
-from flask import flask,render_template,request,redirect,url_for
-import mysql.connector
-app = Flask(__name__)
-
-def get_db_connection():
-    return mysql.connector.connect(
-        host ="localhost",
-        
